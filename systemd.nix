b5d5e77886e2fd53229f5d1140047c0dab6f6901
mow44 <mow44@proton.me>
2025-05-10 23:16:31 +0300

    Rewrite

--- systemd.nix
+++ systemd.nix
@@ -4,12 +4,18 @@
     startServices = true;
 
     services.set-wallpaper = {
-      script = ''
-        	${pkgs.hsetroot}/bin/hsetroot -fill /home/a/NixOS-B550/wallpapers/1.jpg
-      '';
+      Unit = {
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
 
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
+      Service = {
+        ExecStart = "${pkgs.hsetroot}/bin/hsetroot -fill /home/a/NixOS-B550/wallpapers/1.jpg";
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
     };
   };
 }
