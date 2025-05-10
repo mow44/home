4e3b63fc39410cc34a5b3d235c16f197640eb1e9
mow44 <mow44@proton.me>
2025-05-10 23:34:32 +0300

    And another

--- systemd.nix
+++ systemd.nix
@@ -10,8 +10,11 @@
       };
 
       Service = {
+        # ExecStart = ''
+        #   ${pkgs.hsetroot}/bin/hsetroot -fill "$(${pkgs.busybox}/bin/find /home/a/NixOS-B550/wallpapers/ -type f | ${pkgs.busybox}/bin/shuf -n 1)"
+        # '';
         ExecStart = ''
-          ${pkgs.hsetroot}/bin/hsetroot -fill "$(${pkgs.busybox}/bin/find /home/a/NixOS-B550/wallpapers/ -type f | ${pkgs.busybox}/bin/shuf -n 1)"
+          ${pkgs.hsetroot}/bin/hsetroot -fill /home/a/NixOS-B550/wallpapers/2.jpg"
         '';
       };
 
