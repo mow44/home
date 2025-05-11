edf430585503b63852dc5b3b8623df80a98153cd
mow44 <mow44@proton.me>
2025-05-11 14:57:49 +0300

    Systemd update

--- systemd.nix
+++ systemd.nix
@@ -1,4 +1,9 @@
-{ pkgs, wallpapersDir, ... }:
+{
+  inputs,
+  pkgs,
+  wallpapersDir,
+  ...
+}:
 {
   systemd.user = {
     startServices = true;
@@ -14,6 +19,43 @@
         ExecStart = ''
           ${pkgs.runtimeShell} -c '${pkgs.hsetroot}/bin/hsetroot -fill "$(${pkgs.busybox}/bin/find ${wallpapersDir} \( -type f -o -type l \) | ${pkgs.busybox}/bin/shuf -n 1)"'
         '';
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+
+    services.set-refresh-rate = {
+      Unit = {
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        Type = "oneshot";
+        ExecStart = ''
+          ${pkgs.runtimeShell} -c '${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-0 --mode 1920x1080 --rate 75'
+        '';
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+
+    services.start-slstatus = {
+      Unit = {
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = ''
+          ${inputs.slstatus.defaultPackage.x86_64-linux}/bin/slstatus
+        '';
+        Restart = "always";
       };
 
       Install = {
